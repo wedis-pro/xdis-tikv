b03a3702c4006ff435ce39154a1e9320cced66c8
weedge <weege007@gmail.com>
2023-06-08 11:32:03 +0800

    add flush db

--- db_flush.go
+++ db_flush.go
@@ -1,9 +1,80 @@
 package xdistikv
 
-import "context"
+import (
+	"context"
+	"fmt"
+
+	"github.com/tikv/client-go/v2/txnkv/transaction"
+)
 
 // FlushDB flushes the data.
 func (db *DB) FlushDB(ctx context.Context) (drop int64, err error) {
+	dataTypes := []byte{
+		StringType,
+		ListType,
+		HashType,
+		SetType,
+		ZSetType,
+		BitmapType,
+	}
+
+	for _, dataType := range dataTypes {
+		res, err := db.kvClient.GetTxnKVClient().ExecuteTxn(ctx, func(txn *transaction.KVTxn) (value interface{}, err error) {
+			return db.flushType(ctx, txn, dataType)
+		})
+		if err != nil {
+			return 0, err
+		}
+		if res == nil {
+			continue
+		}
+		drop += res.(int64)
+	}
+
 	return
 
+}
+
+func (db *DB) flushType(ctx context.Context, txn *transaction.KVTxn, dataType byte) (drop int64, err error) {
+	var deleteFunc func(ctx context.Context, txn *transaction.KVTxn, key []byte) (int64, error)
+	var metaDataType byte
+	switch dataType {
+	case StringType:
+		deleteFunc = db.string.delete
+		metaDataType = StringType
+	case ListType:
+		deleteFunc = db.list.delete
+		metaDataType = LMetaType
+	case HashType:
+		deleteFunc = db.hash.delete
+		metaDataType = HSizeType
+	case SetType:
+		deleteFunc = db.set.delete
+		metaDataType = SSizeType
+	case ZSetType:
+		deleteFunc = db.zset.delete
+		metaDataType = ZSizeType
+	case BitmapType:
+		deleteFunc = db.bitmap.delete
+		metaDataType = BitmapType
+	default:
+		return 0, fmt.Errorf("invalid data type: %s", TypeName[dataType])
+	}
+
+	var keys [][]byte
+	keys, err = db.scanGeneric(ctx, metaDataType, nil, 1024, false, "", false)
+	for len(keys) != 0 || err != nil {
+		for _, key := range keys {
+			if _, err = deleteFunc(ctx, txn, key); err != nil {
+				return 0, err
+			}
+			if _, err = db.rmExpire(ctx, txn, dataType, key); err != nil {
+				return 0, err
+			}
+		}
+
+		drop += int64(len(keys))
+		keys, err = db.scanGeneric(ctx, metaDataType, nil, 1024, false, "", false)
+	}
+	return
 }
---
+++ db_scan.go
@@ -0,0 +1,92 @@
+package xdistikv
+
+import (
+	"context"
+
+	"github.com/weedge/pkg/utils"
+	"github.com/weedge/xdis-tikv/v1/driver"
+)
+
+func (db *DB) scanGeneric(ctx context.Context, storeDataType byte, key []byte, count int,
+	inclusive bool, match string, reverse bool) ([][]byte, error) {
+
+	r, err := utils.BuildMatchRegexp(match)
+	if err != nil {
+		return nil, err
+	}
+
+	minKey, maxKey, err := db.buildScanKeyRange(storeDataType, key, reverse)
+	if err != nil {
+		return nil, err
+	}
+
+	it, err := db.buildScanIterator(ctx, minKey, maxKey, inclusive, reverse)
+	if err != nil {
+		return nil, err
+	}
+
+	count = checkScanCount(count)
+	v := make([][]byte, 0, count)
+	for i := 0; it.Valid() && i < count; it.Next() {
+		if k, err := db.decodeScanKey(storeDataType, it.Key()); err != nil {
+			continue
+		} else if r != nil && !r.Match(k) {
+			continue
+		} else {
+			v = append(v, k)
+			i++
+		}
+	}
+	it.Close()
+	return v, nil
+}
+
+func (db *DB) buildScanKeyRange(storeDataType byte, key []byte, reverse bool) (minKey []byte, maxKey []byte, err error) {
+	if !reverse {
+		if minKey, err = db.encodeScanMinKey(storeDataType, key); err != nil {
+			return
+		}
+		if maxKey, err = db.encodeScanMaxKey(storeDataType, nil); err != nil {
+			return
+		}
+		return
+	}
+
+	if minKey, err = db.encodeScanMinKey(storeDataType, nil); err != nil {
+		return
+	}
+	if maxKey, err = db.encodeScanMaxKey(storeDataType, key); err != nil {
+		return
+	}
+	return
+}
+
+func checkScanCount(count int) int {
+	if count <= 0 {
+		count = DefaultScanCount
+	}
+
+	return count
+}
+
+func (db *DB) buildScanIterator(ctx context.Context, min []byte, max []byte, inclusive bool, reverse bool) (iter driver.IIterator, err error) {
+	// open (s,e)
+	minKey := append(min, 0)
+	maxKey := max
+
+	// Ropen [s,e)
+	if !reverse && inclusive {
+		minKey = min
+		maxKey = max
+	}
+	// Lopen (s,e]
+	if reverse && inclusive {
+		minKey = append(min, 0)
+		maxKey = append(max, 0)
+	}
+
+	if !reverse {
+		return db.kvClient.GetTxnKVClient().Iter(ctx, nil, minKey, maxKey, 0, -1)
+	}
+	return db.kvClient.GetTxnKVClient().ReverseIter(ctx, nil, minKey, maxKey, 0, -1)
+}

77cf72a9107a5c41c8730ea4bcd5f3580e5dff87
weedge <weege007@gmail.com>
2023-06-22 00:23:35 +0800

    fix
    Signed-off-by: weedge <weege007@gmail.com>

--- tikv/client_txnkv_test.go
+++ tikv/client_txnkv_test.go
@@ -143,3 +143,72 @@ func TestTxnKVClientWrapper_ExecutTxn_Iter(t *testing.T) {
 		t.Fatalf("err %s", err.Error())
 	}
 }
+
+func TestTxnKVClientWrapper_ExecutTxn_Put_Iter(t *testing.T) {
+	TestInitTxnClient(t)
+
+	//strTime := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+	strTime := ""
+	cn := 3
+	key := []byte(strTime + "\x00\x06hitikv\x00\x01\x00\n\x00\azmclear=\x00\x00\x00\x00\x00\x00\x00\x02:m3")
+	val := []byte{000}
+	_, err = kvClient.ExecuteTxn(ctx, func(txn *transaction.KVTxn) (interface{}, error) {
+		for i := 0; i < cn; i++ {
+			//k := append(key, []byte(strconv.Itoa(i))...)
+			k := append(key, byte(i))
+			err = txn.Set(k, val)
+			if err != nil {
+				t.Fatalf("txn set err %s", err.Error())
+			}
+			v, err := txn.Get(ctx, k)
+			if err != nil {
+				t.Fatalf("txn get err %s", err.Error())
+			}
+			if !reflect.DeepEqual(val, v) {
+				t.Fatalf("get %s expected %s", v, val)
+			}
+		}
+		return nil, nil
+	})
+	if err != nil {
+		t.Fatalf("err %s", err.Error())
+	}
+
+	_, err = kvClient.ExecuteTxn(ctx, func(txn *transaction.KVTxn) (interface{}, error) {
+		minKey := []byte(strTime + "\x00\x06hitikv\x00\x01\x00\n\x00\azmclear<\x80\x00\x00\x00\x00\x00\x00\x01:")
+		maxKey := []byte(strTime + "\x00\x06hitikv\x00\x01\x00\n\x00\azmclear=\x7f\xff\xff\xff\xff\xff\xff\xff;")
+		iter, err := kvClient.Iter(ctx, txn, minKey, maxKey, 0, -1)
+		if err != nil {
+			t.Fatalf("iter err %s", err.Error())
+		}
+		_, ok := iter.(*RangeIter)
+		if !ok {
+			t.Fatalf("iter not RangeIter")
+		}
+
+		i := 0
+		for ; iter.Valid(); iter.Next() {
+			ik := iter.Key()
+			//k := append(key, []byte(strconv.Itoa(i))...)
+			k := append(key, byte(i))
+			if !reflect.DeepEqual(ik, k) {
+				t.Fatalf("get %s expected %s", ik, k)
+			}
+			i++
+			err = txn.Delete(ik)
+			if err != nil {
+				t.Fatalf("delete key %s err %s", ik, err.Error())
+			}
+		}
+		iter.Close()
+
+		if !reflect.DeepEqual(cn, i) {
+			t.Fatalf("get %d expected %d", i, cn)
+		}
+
+		return nil, nil
+	})
+	if err != nil {
+		t.Fatalf("err %s", err.Error())
+	}
+}
